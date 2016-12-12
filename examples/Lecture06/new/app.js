13feb2f1d7eb7fc7561354353d80212b81af1ecb
kasunokira <kasunhunt@gmail.com>
2016-12-12 05:56:29 +0000

    lecture06

---
+++ examples/Lecture06/new/app.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+	angular.module("NameCalculator",[])
+	.controller('NameCalculatorController', function ($scope){
+		$scope.name="";
+		$scope.totalValue = 0;
+		
+	});
+
+
+})();
