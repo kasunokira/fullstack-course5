49d9708d2a8891bed06ae1a1299f8b49eb0012eb
kasunokira <kasunhunt@gmail.com>
2016-12-12 06:06:22 +0000

    lecture06 1

--- examples/Lecture06/new/app.js
+++ examples/Lecture06/new/app.js
@@ -5,7 +5,20 @@
 	.controller('NameCalculatorController', function ($scope){
 		$scope.name="";
 		$scope.totalValue = 0;
-		
+		$scope.displayNeumeric = function () {
+			
+			var totalNameValue = calculateNeumericForString($scope.name); //get the total value
+			$scope.totalValue = totalNameValue;
+			
+		};	
+
+		function calculateNeumericForString(string){
+			var totalStringValue = 0;
+    			for (var i = 0; i < string.length; i++) {
+      				totalStringValue += string.charCodeAt(i);
+    			}
+    			return totalStringValue;
+		}	
 	});
 
 
