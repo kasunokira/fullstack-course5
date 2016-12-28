a172b8bd273a42f85aa7a63e5e99278fbaa01bd5
kasunokira <kasunhunt@gmail.com>
2016-12-28 15:23:19 +0000

    Lecture09.3

--- examples/Lecture09/new/app.js
+++ examples/Lecture09/new/app.js
@@ -7,7 +7,7 @@ angular.module('DIApp', [])
 function DIController ($scope, $filter) {
 	$scope.name = "Yaakov";
 	
-	$scope.upper = function{
+	$scope.upper = function(){
 		var upCase = $filter('uppercase');
 		$scope.name = upCase($scope.name);
 	};
